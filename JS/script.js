161280224fbf8a5b3841bb28ac7dbca37f16e176
al3hop <alexhdominguez@gmail.com>
2025-10-24 19:24:55 +0200

    change theme

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,7 @@ let menu_opt = document.querySelector(".menu");
 // Variable para saber si el menu esta abierto o cerrado
 let isOpen = false;
 
-// Definimos evento click sobr eel botón del menú
+// Definimos evento click sobre el botón del menú
 burger.addEventListener("click", function(){
 
     burger.classList.toggle("fa-bars");
@@ -15,3 +15,40 @@ burger.addEventListener("click", function(){
     menu_opt.classList.toggle("menu-show");
 
 });
+
+
+
+// Cambio de modo
+let btnSwitchMode = document.querySelector(".theme-switch");
+
+btnSwitchMode.addEventListener("click", function() {
+    let body = document.querySelector("body");
+    let to_dark_mode = document.querySelector(".to-dark-mode");
+    let to_light_mode = document.querySelector(".to-light-mode");
+    let condition = body.classList.contains("dark-theme");
+
+    console.log(condition);
+
+
+
+    if (condition) {
+        body.classList.remove("dark-theme");
+        body.classList.add("light-theme");
+
+    } else {
+        body.classList.remove("light-theme");
+        body.classList.add("dark-theme");
+
+    }
+
+    to_dark_mode.classList.toggle("hide-cont");
+    to_light_mode.classList.toggle("hide-cont");
+
+
+
+
+    
+
+});
+
+
--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 
 Para definir un evento sobre un elemento de HTML, podemos usar la función built-it de JS llamada -> addEventListener()
 
+## Segunda versión del menú responsive 
+
+
 ## FUNCIÓN built in toggle() de JS
 
+https://www.w3schools.com/howto/howto_js_toggle_class.asp
+
+Esta función me permite añadir o quitar una clase a un ele,mento de HTML. La propia función detecta si tienes oi no tienes la clase y lo añade o la quita. 
